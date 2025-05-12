59e448c4316247b72a978ae53a68cd489a277db6
nataliapc <natalia.pujol@gmail.com>
2025-05-12 13:31:04 +0200

    Updates version to 1.0.2
    Updates the application version number in both the help file and the header file.
    
    Refactors `execv` function improving compatibility and reliability.
    Bugfix: Using shell command for loading the program.

--- src/libs/utils_execv.c
+++ src/libs/utils_execv.c
@@ -1,76 +1,39 @@
 #include <stdint.h>
 #include <string.h>
-#include "msx_const.h"
 #include "dos.h"
 #include "heap.h"
 #include "utils.h"
 
 
-#define LAUNCHER_BASE		0xc000
-
-
-// ========================================================
-static void launcher() __naked
-{
-	__asm
-		/*
-		    OPEN FILE HANDLE (43H)
-		    Parameters:    C = 43H (_OPEN) 
-		                DE = Drive/path/file ASCIIZ string
-		                       or fileinfo block pointer
-		                   A = Open mode. b0 set => no write
-		                                  b1 set => no read
-		                                  b2 set => inheritable   
-		                                  b3..b7 -  must be clear
-		    Results:       A = Error [NOFIL, IATTR, DIRX, NHAND, NORAM, ACCV, FILRO]
-		                   B = New file handle
-		*/
-		ld   hl, (_heap_top)			; Get SHELL filename to read
-		ex   de, hl
-		ld   a, #O_RDONLY				; Open mode: read only
-		ld   c, #OPEN					; DOS Call: OPEN FILE [FCB]
-		DOSCALL
-		/*
-		    READ FROM FILE HANDLE (48H)
-		    Parameters:    C = 48H (_READ) 
-		                   B = File handle
-		                  DE = Buffer address
-		                  HL = Number of bytes to read  
-		    Results:       A = Error [ACCV, EOF]
-		                  HL = Number of bytes actually read
-		*/
-		ld   de, #0x0100				; Address to load the program
-		ld   hl, #0xffff				; Read 0xffff bytes
-		ld   c, #READ					; DOS Call: READ FILE [FCB]
-		DOSCALL
-		/*
-		    CLOSE FILE HANDLE (45H)
-		    Parameters:    C = 45H (_CLOSE) 
-		                   B = File handle
-		    Results:       A = Error
-		*/
-		ld   c, #CLOSE					; DOS Call: CLOSE FILE [FCB]
-		DOSCALL
-
-		ld   sp, (HIMEM)				; Set stack to HIMEM (memory top)
-		jp   0x0100						; Execute the program
-
-	__endasm;
-}
+FILEH executableFile;
 
 // ========================================================
+// Thanks to Laurent Halter for his help with this code
 void execv(char *execStr)
 {
-	//Copy launcher to address 0xd000
-	memcpy((void *)LAUNCHER_BASE, (const void *)launcher, 0x100);
 	//Copy program arguments to 0x0080
 	ADDR_POINTER_BYTE(0x0080) = strlen(execStr);
-	memcpy((void *)0x81, execStr, 80);
+	strncpy((void *)0x81, execStr, 80);
 
-	//Copy program SHELL COMMAND2.COM and location to heap_top
-	char ret =dos2_getEnv("SHELL", (char *)heap_top, 255);
+	//Copy program SHELL (usually A:\COMMAND2.COM) to heap_top
+	dos2_getEnv("SHELL", (char *)heap_top, 255);
+	if (heap_top[0] == 0) {
+		strcpy(heap_top, "A:\\COMMAND2.COM");
+	}
+	
+	executableFile = dos2_fopen((char *)heap_top, O_RDONLY);
+	if (executableFile >= ERR_FIRST) {
+		return;
+	}
 
 	__asm
-		jp LAUNCHER_BASE
+		ld   sp,(0x0006)
+
+		ld   de,#0x100					// DE = Address to load file
+		push de							// Set execution address to 0x0100 when returns from DOSCALL
+		ld   bc,(_executableFile - 1)	// B = file handle
+		ld   hl,#0xFFFF					// HL = size to load from file
+		ld   c,#READ					// C = READ (MSXDOS2 BIOS call)
+		DOSCALL
 	__endasm;
 }
